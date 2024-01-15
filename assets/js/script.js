dbe6c17f685a3d0d382106abf592563de66def71
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-01-15 23:04:12 +0700

    style: calculator display

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const primaryDisplay = document.getElementById("primaryDisplay");
+const secondaryDisplay = document.getElementById("secondaryDisplay");
+
+primaryDisplay.innerText = "123";
+secondaryDisplay.innerText = "123 + 456";

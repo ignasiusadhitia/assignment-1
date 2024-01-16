a5729cfcedeb304e0fee6fe69c6c18b8507f370c
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-01-16 19:30:46 +0700

    feat: display entry and calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,83 @@
+let currentEntry = "";
+let firstOperand = "";
+let operator = "";
+let secondOperand = "";
+let result = "";
+
+// Displays
 const primaryDisplay = document.getElementById("primaryDisplay");
 const secondaryDisplay = document.getElementById("secondaryDisplay");
 
-primaryDisplay.innerText = "123";
-secondaryDisplay.innerText = "123 + 456";
+// Displays initial state
+primaryDisplay.innerText = "0";
+secondaryDisplay.innerText = "";
+
+// Buttons
+const operands = document.querySelectorAll(".operand");
+const operators = document.querySelectorAll(".operator");
+const resultButton = document.getElementById("=");
+const allClearButton = document.getElementById("ac");
+const clearEntryButton = document.getElementById("c");
+
+// Operand button event listener
+operands.forEach(function (operand) {
+  operand.addEventListener("click", function () {
+    const value = operand.getAttribute("data-value");
+    currentEntry += value;
+    showEntry(currentEntry);
+  });
+});
+
+// Operator button event listener
+operators.forEach(function (operator) {
+  operator.addEventListener("click", function () {
+    const operation = operator.getAttribute("data-value");
+    setFirstOperand(operation);
+  });
+});
+
+// resultButton event listener
+resultButton.addEventListener("click", function () {
+  setSecondOperand();
+});
+
+// All Clear button event listener
+allClearButton.addEventListener("click", function () {
+  allClear();
+});
+
+// Clear entry button event listener
+clearEntryButton.addEventListener("click", function () {
+  clearEntry();
+});
+
+function showEntry(value) {
+  primaryDisplay.innerText = value;
+}
+
+function setFirstOperand(operation) {
+  firstOperand = result || currentEntry || "0";
+  operator = operation;
+  secondaryDisplay.innerText = firstOperand + " " + operation;
+  currentEntry = "";
+}
+
+function setSecondOperand() {
+  secondOperand = currentEntry || firstOperand;
+  secondaryDisplay.innerText =
+    firstOperand + " " + operator + " " + secondOperand + " " + "=";
+  currentEntry = "";
+}
+
+function calculate() {}
+
+function allClear() {
+  currentEntry = "";
+  primaryDisplay.innerText = "0";
+  secondaryDisplay.innerText = "";
+}
+
+function clearEntry() {
+  currentEntry = "";
+  primaryDisplay.innerText = "0";
+}
